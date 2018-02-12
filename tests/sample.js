a1451580d187f84244ebc192e0daac9ae13e074c
kazuhisa <hcz5cnorihrl93nvm8@gmail.com>
2018-02-12 23:48:44 +0900

    first commit

---
+++ tests/sample.js
@@ -0,0 +1,51 @@
+//var chromedriver = require('chromedriver');
+//module.exports = {
+//  before : function(done) {
+//    chromedriver.start();
+//
+//    done();
+//  },
+//
+//  after : function(done) {
+//    chromedriver.stop();
+//
+//    done();
+//  }
+//};
+
+module.exports = {
+  'Demo test' : function (browser) {
+
+  console.log(browser.globals);
+        browser
+        .url(browser.launchUrl)
+        .waitForElementPresent('a[class="wg-header__login-button wg-btn wg-btn--white wg-btn--hollow"]', 1000)
+        .click('form:first-child > a[class="wg-header__login-button wg-btn wg-btn--white wg-btn--hollow"]')
+        .waitForElementPresent('button[class="button-light-blue"]', 5000)
+        .setValue('#emailField', 'pd67ac8fd7xrdmzz@gmail.com')
+        .setValue('#passwordField', 'k4zuhisa')
+        .click('#submit-login-button')
+        .waitForElementPresent('div[class="wspace-tree-title-text"]', 5000)
+        .useXpath()
+        .click('//div/a/div/div[text()="ああああ"]') // 最上層のディレクトリ
+        .pause(1000)
+        .click('//div/div[text()="01あああ"]') // 2階層
+        .pause(1000)
+        .click('//div/div[text()="１１１"]') // 3階層
+        .pause(1000)
+        .pause(5000)
+        .useCss()
+        .source(function(html){
+            browser
+                .useXpath()
+                .getValue('//*[@id="wspace;lister;group=later"]/div[2]')
+//            html.wrike-task-list-view-group-items
+        })
+        .end();
+  // {
+  //   "myGlobalVar" : "some value",
+  //   "otherGlobal" : "some other value"
+  // }
+
+  }
+};
