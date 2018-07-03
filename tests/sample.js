55d5d5c4b71272a76f98e9f7355b138f321480db
lazyarea <lazyarea@localhost.localdomain>
2018-07-04 08:41:27 +0900

    aaa

--- tests/sample.js
+++ tests/sample.js
@@ -19,26 +19,13 @@ module.exports = {
   console.log(browser.globals);
         browser
         .url(browser.launchUrl)
-        .waitForElementPresent('a[class="wg-header__login-button wg-btn wg-btn--white wg-btn--hollow"]', 1000)
-        .click('form:first-child > a[class="wg-header__login-button wg-btn wg-btn--white wg-btn--hollow"]')
-        .waitForElementPresent('button[class="button-light-blue"]', 5000)
-        .setValue('#emailField', 'user-email@gmail.com')
-        .setValue('#passwordField', 'password')
-        .click('#submit-login-button')
-        .waitForElementPresent('div[class="wspace-tree-title-text"]', 5000)
-        .useXpath()
-        .click('//div/a/div/div[text()="ああああ"]') // 最上層のディレクトリ
-        .pause(1000)
-        .click('//div/div[text()="01あああ"]') // 2階層
-        .pause(1000)
-        .click('//div/div[text()="１１１"]') // 3階層
-        .pause(1000)
+#        .waitForElementPresent('a[class="wg-header__login-button wg-btn wg-btn--white wg-btn--hollow"]', 1000)
         .pause(5000)
         .useCss()
         .source(function(html){
             browser
-                .useXpath()
-                .getValue('//*[@id="wspace;lister;group=later"]/div[2]')
+#                .useXpath()
+#                .getValue('//*[@id="wspace;lister;group=later"]/div[2]')
 //            html.wrike-task-list-view-group-items
         })
         .end();
